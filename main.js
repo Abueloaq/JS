7a5ce99c8f3cec0455dd992fc3a45b747e6b3ca7
Abueloaq <jorgewillebald@gmail.com>
2022-02-23 20:04:59 -0300

    acumulador

---
+++ main.js
@@ -0,0 +1,11 @@
+// true o false
+
+const nombresDeProductos = ['Zapa1', 'Zapa niky', 'Adidas'];
+
+let acumulador = "";
+
+for (let i = 0; i < 3; i++) {
+    acumulador += nombresDeProductos[i];
+}
+
+document.write(acumulador);
