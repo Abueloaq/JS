328c704125ee095450cd7e924f10ebc7c17c2d08
Abueloaq <jorgewillebald@gmail.com>
2022-02-23 22:20:49 -0300

    primer desafío

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-// true o false
 
-const nombresDeProductos = ['Zapa1', 'Zapa niky', 'Adidas'];
 
-let acumulador = "";
+const notasFinales = [9, 8, 10, 7, 3, 6, 5]
 
-for (let i = 0; i < 3; i++) {
-    acumulador += nombresDeProductos[i];
+for (let i = 0; i < 7; i++) {
+    if (notasFinales[i] >= 6) {
+        console.log('El alumno aprobó la prueba final')
+    } else {
+        console.log('El alumno no aprobó la prueba final')
+    }
 }
 
-document.write(acumulador);
+
+
+
+
+
